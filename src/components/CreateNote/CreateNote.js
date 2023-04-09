7d8731b3db2ab9c383cd5c1b0e906b05e59acca4
Michee_Tokatoka <tokatokakiyombo@gmail.com>
2023-04-09 10:21:49 +0200

    added create note and reorganised files

--- src/components/CreateNote/CreateNote.js
+++ src/components/CreateNote/CreateNote.js
@@ -1,40 +1,42 @@
 import "../Notes/Notes.css";
 
 function createNote(note) {
-  return `
-            <div class="create-note" id="${note.id}">
-            <span class="material-icons check-circle"> check_circle </span>
-            <div class="create-note--title">${note.title}</div>
-            <div class="create-note--text">${note.text}</div>
+  return (
+    <>
+      <div class="create-note" id={note.id}>
+        <span class="material-icons check-circle"> check_circle </span>
+        <div class="create-note--title">{note.title}</div>
+        <div class="create-note--text">{note.text}</div>
 
-            <div class="create-note--footer">
-              <div class="tooltip">
-                <i class="material-icons hover">add_alert</i>
-                <span class="tooltip-text">Remind me</span>
-              </div>
-              <div class="tooltip">
-                <i class="material-icons hover">person_add</i>
-                <span class="tooltip-text">Collaborator</span>
-              </div>
-              <div class="tooltip">
-                <i class="material-icons hover">palette</i>
-                <span class="tooltip-text">Background options</span>
-              </div>
-
-              <div class="tooltip">
-                <i class="material-icons hover">image</i>
-                <span class="tooltip-text">Add Image</span>
-              </div>
-              <div class="tooltip archive">
-                <i class="material-icons hover">archive</i>
-                <span class="tooltip-text">Archive</span>
-              </div>
-              <div class="tooltip">
-                <i class="material-icons hover">more_vert</i>
-                <span class="tooltip-text">More</span>
-              </div>
-            </div>
+        <div class="create-note--footer">
+          <div class="tooltip">
+            <i class="material-icons hover">add_alert</i>
+            <span class="tooltip-text">Remind me</span>
           </div>
-    `;
+          <div class="tooltip">
+            <i class="material-icons hover">person_add</i>
+            <span class="tooltip-text">Collaborator</span>
+          </div>
+          <div class="tooltip">
+            <i class="material-icons hover">palette</i>
+            <span class="tooltip-text">Background options</span>
+          </div>
+
+          <div class="tooltip">
+            <i class="material-icons hover">image</i>
+            <span class="tooltip-text">Add Image</span>
+          </div>
+          <div class="tooltip archive">
+            <i class="material-icons hover">archive</i>
+            <span class="tooltip-text">Archive</span>
+          </div>
+          <div class="tooltip">
+            <i class="material-icons hover">more_vert</i>
+            <span class="tooltip-text">More</span>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 export default createNote;
