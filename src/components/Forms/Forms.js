e319b4211c4b677ecb639dad517fd50ee97d1aa5
Michee_Tokatoka <tokatokakiyombo@gmail.com>
2023-04-10 13:05:51 +0200

    used conditional rendering instead of ternary

--- src/components/Forms/Forms.js
+++ src/components/Forms/Forms.js
@@ -7,10 +7,5 @@ import FormActive from "./FormActive";
 export default function Forms() {
   const [openActiveForm, setopenActiveForm] = useState(false);
   const handleInputClick = () => setopenActiveForm(true);
-  return (
-    <>
-      <FormInactive inputClick={handleInputClick} />
-      {openActiveForm && <FormActive />}
-    </>
-  );
+  return <>{openActiveForm ? <FormActive /> : <FormInactive inputClick={handleInputClick} />}</>;
 }
